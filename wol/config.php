b77012dbffb2d3a8701f5e910047d44991d221b8
ymch <2923124+ymch@users.noreply.github.com>
2025-12-28 11:28:01 +0900

    Change license to MIT and update comments
    Updated license from GPL to MIT and adjusted comments.

--- wol/config.php
+++ wol/config.php
@@ -4,8 +4,8 @@
  *
  * This file is part of the wol.zip package.
  *
- * This software is licensed under the GNU General Public License (GPL).
- * You can redistribute it and/or modify it under the terms of the GPL.
+ * This software is licensed under the MIT License.
+ * See the LICENSE file for details.
  *
  * This software is provided as-is, without any warranty.
  * The author assumes no responsibility for any damages or issues arising from its use.
@@ -32,3 +32,4 @@ define('WOL_PORT', 2304); // 2304 or 9
 // ===== Log File =====
 define('WOL_LOG_FILE', __DIR__ . '/wol.log');
 define('WOL_LOG_MAX', 100); // Number of Log Entries Saved
+
